bd8e5e7f46c5926a9a0f6271976779a818264085
Rafael Lorandi <coolparadox@gmail.com>
2025-08-23 19:57:34 +0200

    generate notes for single section

--- make_deck.sh
+++ make_deck.sh
@@ -9,15 +9,28 @@ fail() {
 }
 
 log() {
+    return
     echo "$ME: $*" >&2
 }
 
-test $# -gt 0 || fail "missing seed"
-SEED=$1
+test $# -gt 0 || {
+    # Just output notes header
+    cat <<__eod__
+#separator:tab
+#html:true
+#notetype column:1
+#deck column:2
+#tags column:11
+__eod__
+    exit 0
+}
 
-QUESTIONS_PATH="${SEED}.questions"
-ANSWERS_PATH="${SEED}.answers"
+# Produce notes for a section
+SECTION=$1
+QUESTIONS_PATH="${SECTION}.questions"
+ANSWERS_PATH="${SECTION}.answers"
 
+# Parse section questions
 declare ID  # question / answer identification
 declare -a QT  # all question texts
 declare -a QA  # all question options a)
@@ -40,20 +53,62 @@ while read CONTEXT CONTENT ; do
         *) fail "invalid input: $CONTEXT"
     esac
 done <$QUESTIONS_PATH
-NQ=${#QT[@]}
+NQ=${#QT[@]}  # Amount of questions
 log "$NQ questions found"
 test $NQ -gt 0 || fail "no questions found"
 
+# Parse section answers
 declare -a ANS  # all answers
 while read ID OPTION ; do
     set +u ; test -n "${QT[$ID]}" || fail "missing question for answer $ID"
     set -u
     case "$OPTION" in
-        'a') test -n "${QA[$ID]}" || fail "missing text for option (a) in question $ID" ;;
-        'b') test -n "${QB[$ID]}" || fail "missing text for option (b) in question $ID" ;;
-        'c') test -n "${QC[$ID]}" || fail "missing text for option (c) in question $ID" ;;
+        'a') test -n "${QA[$ID]}" || fail "missing option (a) in question $ID" ;;
+        'b') test -n "${QB[$ID]}" || fail "missing option (b) in question $ID" ;;
+        'c') test -n "${QC[$ID]}" || fail "missing option (c) in question $ID" ;;
         *) fail "unknown answer '$OPTION' for question $ID" ;;
     esac
     ANS[$ID]=$OPTION
 done <<<$( tr -d '\n' <$ANSWERS_PATH | sed -E -e 's|([[:alpha:]])([[:digit:]])|\1\n\2|g' -e '$a\' )
+NA=${#ANS[@]}  # Amount of answers
+log "$NA answers found"
+test $NA -eq $NQ || fail "Mismatching amount of questions ($NQ) and answers ($NA)"
+for ID in ${!QT[@]} ; do
+    set +u ; test -n "${ANS[$ID]}" || fail "missing answer for question $ID"
+    set -u
+done
+
+# Generate Anki notes for section.
+echo_column_back_option() {
+    local OPTION_LETTER=$1
+    local OPTION_TEXT=$2
+    local CORRECT_OPTION=$3
+    echo -en "\t"
+    test -n "$OPTION_TEXT" || return
+    echo -n '"'
+    echo -n '<div class=""'
+    if test "$OPTION_LETTER" = "$CORRECT_OPTION" ; then
+        echo -n 'yes'
+    else
+        echo -n 'no'
+    fi
+    echo -n '"">'
+    echo -n $OPTION_TEXT
+    echo -n '</div>'
+    echo -n '"'
+}
+for ID in ${!QT[@]} ; do
+    echo -n 'choice_abc'
+    echo -en '\tccse'
+    echo -en "\t${ID}"
+    echo -en "\t${QT[$ID]}"
+    echo -en "\t${QA[$ID]}"
+    echo -en "\t${QB[$ID]}"
+    echo -en "\t${QC[$ID]}"
+    echo_column_back_option 'a' "${QA[$ID]}" "${ANS[$ID]}"
+    echo_column_back_option 'b' "${QB[$ID]}" "${ANS[$ID]}"
+    echo_column_back_option 'c' "${QC[$ID]}" "${ANS[$ID]}"
+    echo -en "\t${SECTION}"
+    echo
+done
 
